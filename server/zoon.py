4838f749bb14efaa6c29e8e0d0a41bddd5e7c957
demend2607 <finder2607011@gmail.com>
2025-09-29 14:17:45 +1000

    successful get whole page

--- server/zoon.py
+++ server/zoon.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 import requests
@@ -10,32 +14,43 @@ import multiprocessing
 
 zoon_link = 'https://zoon.ru/khabarovsk/trainings/type/shkola_tantsev/'
 
-def get_source_html(url, page):
-    
-    driver = webdriver.Firefox()
-    driver.maximize_window()
+def get_source_html(url):
 
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
     try:
-        driver.get(f'{url}page-{page}/')
-        time.sleep(random.uniform(1, 4))
-        with open('zoon/zoon.html', 'w', encoding='utf-8') as file:
-            file.write(driver.page_source)
+        driver.get(url)
+        time.sleep(6)
         
+        while True:
+            # button-show-more
+            catalog_button = driver.find_element(By.CLASS_NAME, 'catalog-button-showMore')
+            if driver.find_elements(By.CLASS_NAME, 'hasmore-text'):
+                with open('zoon/zoon.html', 'w', encoding='utf-8') as file:
+                    file.write(driver.page_source)
+                break
+            else:
+                button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'button-show-more')))
+                actions = ActionChains(driver)
+                
+                actions.click(button).perform()
+                actions.move_to_element(catalog_button).pause(2).perform()
+
+                time.sleep(3)                
         return True
     except Exception as error:
         print(f'Error has occurred: {error}')
-        return False
     finally:
         driver.close()
         driver.quit()
+        
+    def get_items_urls():
+        return True
 
 def main():
-    # for page in range(1 , 6):
-        success = get_source_html(zoon_link, page=1)
-        # if not success:
-        #     print('error')
-        #     break
-        # time.sleep(random.uniform(1, 3))
+    get_source_html(zoon_link)
+
     
 
 if __name__ == '__main__':
