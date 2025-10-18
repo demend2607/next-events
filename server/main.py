1a032de722f2d824a435aaaec1bf031641d224df
Demend <finder2607011@gmail.com>
2025-10-18 14:34:47 +1000

    ignore file update

--- server/main.py
+++ server/main.py
@@ -11,39 +11,9 @@ import multiprocessing
 app = Flask(__name__)
 CORS(app)
 
-def proxy_handler(proxy):
-    time.sleep(random.uniform(0.7, 3.5))
-    link = f"http://icanhazip.com/"
-    
-    proxies = {
-        'http': f'http://{proxy}',
-        'https': f'https://{proxy}'
-    }
-    
-    try:
-        response = requests.get(link, proxies=proxies, timeout=2).text
-        print(f'id: {response}')
-        return proxy
-    except:
-        print('not')
-        return None
-
-def multi_proxy_check():
-    with open('proxy/Raw.txt', 'r') as file:
-        proxy_base = ''.join(file.readlines()).strip().split('\n')
-        
-    with multiprocessing.Pool(multiprocessing.cpu_count()) as process:
-        results = process.map(proxy_handler, proxy_base)
-        
-    valid_proxy = [proxy for proxy in results if proxy is not None]
-        
-    
-    with open('proxy/Valid.txt', 'w') as file:
-        file.write('\n'.join(valid_proxy))
 
 @app.route('/api/home', methods=['GET'])
 def return_home():
-    print(multi_proxy_check())
     
     with open('proxy/Valid.txt') as file:
         proxy_base = ''.join(file.readlines()).strip().split('\n')
